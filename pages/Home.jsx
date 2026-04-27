993aa9ab3da864197dd35238262dd6518f78349f
chouaib <chouaibbenchouaib0@gmail.com>
2026-04-27 09:52:56 +0100

    editting

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -105,8 +105,8 @@ const Home = ({ onNavigate }) => {
           </button>
         </div>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-          {PROJECTS.slice(0, 2).map((project) => (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          {PROJECTS.slice(0, 3).map((project) => (
             <ProjectCard
               key={project.id}
               project={project}
